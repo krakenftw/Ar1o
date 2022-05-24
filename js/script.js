933d49a1bb0380066d2f2707b3c316520822b724
kraken <solodarken@gmail.com>
2022-05-24 22:00:06 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-window.addEventListener('resize', function(){
+window.addEventListener('resize', function () {
     addRequiredClass();
 })
 
@@ -8,7 +8,7 @@ function addRequiredClass() {
     if (window.innerWidth < 860) {
         document.body.classList.add('mobile')
     } else {
-        document.body.classList.remove('mobile') 
+        document.body.classList.remove('mobile')
     }
 }
 
@@ -21,9 +21,9 @@ let bars = document.querySelectorAll('.hamburger span')
 
 let isActive = false
 
-hamburger.addEventListener('click', function() {
+hamburger.addEventListener('click', function () {
     mobileNav.classList.toggle('open')
-    if(!isActive) {
+    if (!isActive) {
         bars[0].style.transform = 'rotate(45deg)'
         bars[1].style.opacity = '0'
         bars[2].style.transform = 'rotate(-45deg)'
@@ -34,7 +34,6 @@ hamburger.addEventListener('click', function() {
         bars[2].style.transform = 'rotate(0deg)'
         isActive = false
     }
-    
+
 
 })
-easy_background(".slideshow", 3000, "img/slideshow1.jpg", 3000, "img/slideshow2.jpg", 3000, "img/slideshow3.jpg");
